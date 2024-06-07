84707cfad265f58c12d29dfc83cc41e3549c44f0
DevEn <kangingoo31@gmail.com>
2024-06-07 18:11:27 +0900

    .

---
+++ src/ex08class/QuSimpleCalculator.java
@@ -0,0 +1,18 @@
+package ex08class;
+
+/*
+ * 문제1) 파일명 : QuSimpleCalculator.java (난이도:하)
+ * 기본 4칙연산을 할때마다 연산의 결과와 몇번 수행되었는지를
+   출력하는 계산기 클래스를 정의해보자.
+ * 아래 main함수에 부합하는 CalculatorEx 클래스를 정의하면되고
+   해당 수치는 임의로 바꿔도 된다.
+   init() 멤버메소드에서는 계산의 결과를 카운트하는 멤버변수를 0으로 초기화 한다.
+ */
+public class QuSimpleCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
