6e48f7b4cbc40a5ccc94d70e55b0e62a8cf8433e
DevEn <kangingoo31@gmail.com>
2024-06-07 17:48:42 +0900

    24.06.07(Fri)_class

--- src/ex12inheritance/E07RideAndLoad01.java
+++ src/ex12inheritance/E07RideAndLoad01.java
@@ -12,6 +12,7 @@ package ex12inheritance;
    즉 오버라이딩은 참조변수의 타입에 영향을 받지 않는다.
  */
 class A {
+	// 생성자는 컴파일러에 의해 자동으로 생성된 상태
 	public void rideMethod() {
 		System.out.println("A의 rideMethod");
 	}
@@ -100,6 +101,42 @@ public class E07RideAndLoad01 {
 		ref1.loadMethod();
 //		ref1.loadMethod(1);
 //		ref1.loadMethod(1.1);
+		
+		
+		
+		
+/////////////////////////////////////////////////////////////////
+		/*
+		 * 부모의 참조변수로 자식의 인스턴스를 참조할 수 있다.
+		 * 좌측은 부모, 우측은 자식이므로 모두 가능한 문장이다.
+		 */
+		A refNum1 = new B();
+		A refNum2 = new C();
+		B refNum3 = new C();
+		
+		// 같은 타입이므로 참조가능
+		C refAddr1 = new C();
+		// B타입 -> C타입을 참조하므로 가능
+		B refAddr2 = refAddr1;
+		// A타입 -> C타입을 참조하므로 가능
+		A refAddr3 = refAddr1;
+		
+		// A -> C 참조이므로 가능
+		A refId1 = new C();
+		// refId1은 A타입이다. B -> A이므로 불가능(자식이 부모를 참조할 수 없다)
+//		B refId2 = refId1;
+		// C -> A이므로 불가능
+//		C refId3 = refId1;
+		
+		/*
+		 * 자식으로 부모인스턴스를 참고하는 것은 기본적으로 불가능하나
+		   다운캐스팅(DownCasting, 강제형변환)을 하면 참조가 가능해진다.
+		   즉 부모타입을 자식타입으로 변경하는 것이다. 이는 클래스가 상속 관계에
+		   있기 때문에 가능하고, 방식은 기본자료형과 동일하게 소괄호를 이용하면
+		   된다.
+		 */
+		B refId2 = (B)refId1;
+		C refId3 = (C)refId1;
 	}
 
 }
